7d10b797f653abb243126ac0128e2c40619753b4
metamaker <admin@metamaker.pro>
2016-07-12 23:22:16 +0300

    Removed not used classes from use section.

--- src/AppBundle/Controller/AlbumController.php
+++ src/AppBundle/Controller/AlbumController.php
@@ -2,10 +2,8 @@
 
 namespace AppBundle\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class AlbumController extends Controller
--- src/AppBundle/Controller/ImageController.php
+++ src/AppBundle/Controller/ImageController.php
@@ -2,11 +2,8 @@
 
 namespace AppBundle\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 
 class ImageController extends Controller
 {
